2e3efeb63b5f8cdcbcf366baefadcf0c75fb2d9e
Mickael MAS <mickael.mas@appiwedia.com>
2020-03-04 16:47:59 +0100

    Demo Toggle Component

--- ToggleComponent/ContentView.swift
+++ ToggleComponent/ContentView.swift
@@ -9,8 +9,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isOptionActivated = false
+    
+    @State private var isFavoriteActivated = false
+    @State private var isTrashActivated = false
+    @State private var isSendActivated = false
+    @State private var isDisturbActivated = false
+
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            Section {
+                Toggle(isOn: $isOptionActivated, label: {
+                    Text("Voir les options")
+                })
+            }
+            
+            if isOptionActivated {
+                Section {
+                Toggle(isOn: $isFavoriteActivated, label: {
+                    HStack {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                        Text("Favoris")
+                    }
+                })
+                
+                Toggle(isOn: $isTrashActivated, label: {
+                    HStack {
+                        Image(systemName: "trash.fill")
+                        .foregroundColor(.red)
+                        Text("Corbeille")
+                    }
+                })
+                
+                Toggle(isOn: $isSendActivated, label: {
+                    HStack {
+                        Image(systemName: "paperplane.fill")
+                        .foregroundColor(.blue)
+                        Text("Envoi")
+                    }
+                })
+                
+                Toggle(isOn: $isDisturbActivated, label: {
+                    HStack {
+                        Image(systemName: "moon.fill")
+                        .foregroundColor(.purple)
+                        Text("Ne pas déranger")
+                    }
+                })
+            }
+            }
+        }
     }
 }
 
